38aacea3b22d8c335fb3b9662136624281afb83f
Erwan Chesnay <erwan.chesnay@outlook.fr>
2018-03-11 14:40:22 +0100

    Connexion à l'espace client

--- connexion_espace_perso/connexion.php
+++ connexion_espace_perso/connexion.php
@@ -7,5 +7,19 @@
     <title></title>
   </head>
   <body>
+    <form>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Votre identifiant email</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Votre mot de passe (6 caractères minimum)</label>
+        <input type="password" pattern=".{6,}" required title="6 caractères minimum" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        <div>
+          Vous avez oublié votre mot de passe : <a href="#">Cliquez ici</a>
+        </div>
+      </div>
+      <button type="submit" class="btn btn-primary">Connexion</button>
+    </form>
   </body>
 </html>
